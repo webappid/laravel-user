e5b64b0b0c18251e6eb43ff986c178abe8660940
Dyan Galih <dyan.galih@gmail.com>
2019-01-30 15:50:47 +0700

    Add RoleCheck MiddleWare

---
+++ src/Middleware/RoleCheck.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dyangalih
+ * Date: 2019-01-30
+ * Time: 15:49
+ */
+
+namespace WebAppId\User\Middleware;
+
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class RoleCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @param string $userRole
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $userRole = "member")
+    {
+        $roles = Auth::user()->roles;
+        $access = false;
+        
+        $userRoles = explode('|', $userRole);
+        
+        foreach ($roles as $role) {
+            foreach ($userRoles as $userRole) {
+                if (strtolower($role->name) == $userRole) {
+                    $access = true;
+                }
+            }
+        }
+        if ($access) {
+            return $next($request);
+        } else {
+            return abort(403);
+        }
+    }
+}
