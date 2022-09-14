22d9f0f4f13e74adbaddae1f82d2ff0f37290270
Dyan Galih <dyan.galih@gmail.com>
2022-09-15 04:53:56 +0700

    Fix on set role api

--- src/routes.php
+++ src/routes.php
@@ -5,7 +5,7 @@ $controller = 'WebAppId\User\Controllers';
 Route::name('api.')->prefix('api')->group(function () use ($controller) {
     Route::post('/login', $controller . '\AuthController@login')->name('login');
     Route::get('/route-auto-collect', $controller . '\Routes\RouteAutoCollectController')->name('login');
-    Route::group(['middleware' => ['auth:sanctum', 'auth.role']], function () use ($controller) {
+    Route::group(['middleware' => ['auth:sanctum', 'role:admin']], function () use ($controller) {
         Route::get('/profile', function(Request $request) {
             return auth()->user();
         })->name('profile');
