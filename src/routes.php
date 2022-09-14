b645338a36f8093cf487848de7b491ea4d5f2508
Dyan Galih <dyan.galih@gmail.com>
2022-09-15 04:58:10 +0700

    Fix auto collect route name

--- src/routes.php
+++ src/routes.php
@@ -4,7 +4,7 @@ $controller = 'WebAppId\User\Controllers';
 
 Route::name('api.')->prefix('api')->group(function () use ($controller) {
     Route::post('/login', $controller . '\AuthController@login')->name('login');
-    Route::get('/route-auto-collect', $controller . '\Routes\RouteAutoCollectController')->name('login');
+    Route::get('/route-auto-collect', $controller . '\Routes\RouteAutoCollectController')->name('auto.collect');
     Route::group(['middleware' => ['auth:sanctum', 'role:admin']], function () use ($controller) {
         Route::get('/profile', function(Request $request) {
             return auth()->user();
